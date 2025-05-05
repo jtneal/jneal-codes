70d9bcf6efd11d22dad229a99d1b1ca9d5b84c6d
Jason Neal <5340961+jtneal@users.noreply.github.com>
2025-05-05 19:50:46 -0400

    Add styles

--- libs/sudoku/src/index.ts
+++ libs/sudoku/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/logic';
 export * from './lib/shuffle';
+export * from './lib/styles';
 export * from './lib/sudoku';
 export * from './lib/types';
---
+++ libs/sudoku/src/lib/styles.ts
@@ -0,0 +1,37 @@
+export const styles = `
+  .sudoku-game {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+  }
+
+  .sudoku-card {
+    background-color: #000;
+    border: 6px solid #000;
+    display: grid;
+    gap: 3px;
+    grid-template-columns: repeat(9, 3rem);
+    grid-template-rows: repeat(9, 3rem);
+    width: fit-content;
+  }
+
+  .sudoku-cell {
+    align-items: center;
+    background-color: #fff;
+    display: flex;
+    font-size: 1.5rem;
+    justify-content: center;
+
+    &:nth-child(9n+3),
+    &:nth-child(9n+6) {
+      margin-right: 3px;
+    }
+
+    &:nth-child(n+19):nth-child(-n+27),
+    &:nth-child(n+46):nth-child(-n+54) {
+      margin-bottom: 3px;
+    }
+  }
+`;
