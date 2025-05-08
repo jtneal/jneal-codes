2c8f54c805a890e4ceb5ed23748399a05d9b0949
Jason Neal <5340961+jtneal@users.noreply.github.com>
2025-05-07 21:36:14 -0400

    Styling for simplicity

--- libs/sudoku/src/lib/styles.ts
+++ libs/sudoku/src/lib/styles.ts
@@ -5,69 +5,69 @@ export const styles = `
     flex-direction: column;
     height: 100vh;
     justify-content: center;
-  }
 
-  .sudoku-header {
-    align-items: center;
-    display: flex;
-    font-family: 'Courier New', Courier, monospace;
-    justify-content: space-between;
-    width: 468px;
+    header {
+      align-items: center;
+      display: flex;
+      font-family: 'Courier New', Courier, monospace;
+      justify-content: space-between;
+      width: 468px;
 
-    h2 {
-      width: 100px;
-    }
-  }
+      h2 {
+        width: 100px;
 
-  .sudoku-score {
-    color: #f00;
-    text-align: right;
-  }
-
-  .sudoku-card {
-    background-color: #000;
-    border: 6px solid #000;
-    display: grid;
-    gap: 3px;
-    grid-template-columns: repeat(9, 3rem);
-    grid-template-rows: repeat(9, 3rem);
-    margin-bottom: 1rem;
-    width: fit-content;
-  }
-
-  .sudoku-cell {
-    align-items: center;
-    background-color: #fff;
-    display: flex;
-    font-size: 1.5rem;
-    justify-content: center;
-
-    &:nth-child(9n+3),
-    &:nth-child(9n+6) {
-      margin-right: 3px;
+        &:last-child {
+          color: #f00;
+          text-align: right;
+        }
+      }
     }
 
-    &:nth-child(n+19):nth-child(-n+27),
-    &:nth-child(n+46):nth-child(-n+54) {
-      margin-bottom: 3px;
+    main {
+      background-color: #000;
+      border: 6px solid #000;
+      display: grid;
+      gap: 3px;
+      grid-template-columns: repeat(9, 3rem);
+      grid-template-rows: repeat(9, 3rem);
+      margin-bottom: 1rem;
+      width: fit-content;
+
+      article {
+        align-items: center;
+        background-color: #fff;
+        display: flex;
+        font-size: 1.5rem;
+        justify-content: center;
+
+        &:nth-child(9n+3),
+        &:nth-child(9n+6) {
+          margin-right: 3px;
+        }
+
+        &:nth-child(n+19):nth-child(-n+27),
+        &:nth-child(n+46):nth-child(-n+54) {
+          margin-bottom: 3px;
+        }
+      }
+
+      input {
+        font-size: 1.4rem;
+        height: 2.5rem;
+        text-align: center;
+        width: 2.5rem;
+      }
     }
-  }
 
-  .sudoku-input {
-    font-size: 1.4rem;
-    height: 2.5rem;
-    text-align: center;
-    width: 2.5rem;
-  }
+    footer {
+      align-items: center;
+      display: flex;
+      justify-content: space-between;
+      width: 468px;
 
-  .sudoku-footer {
-    align-items: center;
-    display: flex;
-    justify-content: space-between;
-    width: 468px;
-
-    select {
-      padding: 0.333rem;
+      select {
+        padding: 0.333rem;
+      }
     }
   }
 `;
