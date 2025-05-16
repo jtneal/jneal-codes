659c0a961e88ea91dad13fc361c0e8b351aed4fc
Jason Neal <5340961+jtneal@users.noreply.github.com>
2025-05-15 20:53:55 -0400

    Fix tests

--- libs/exchange-rates-sdk/src/lib/exchange-rates.module.ts
+++ libs/exchange-rates-sdk/src/lib/exchange-rates.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExchangeRatesService } from './exchange-rates.service';
 import { ExchangeRatesServiceMock } from './exchange-rates.service.mock';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   exports: [ExchangeRatesService, ExchangeRatesServiceMock],
+  imports: [HttpModule],
   providers: [ExchangeRatesService, ExchangeRatesServiceMock],
 })
 export class ExchangeRatesModule {}
--- libs/exchange-rates-sdk/src/lib/exchange-rates.service.spec.ts
+++ libs/exchange-rates-sdk/src/lib/exchange-rates.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { ExchangeRatesService } from './exchange-rates.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('ExchangeRatesSdkService', () => {
   let service: ExchangeRatesService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [ExchangeRatesService],
     }).compile();
 
